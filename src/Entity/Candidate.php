ae05058c9ecf42df52c608453643d4f3f77e5ab8
Esther Itam <esther.itam@live.fr>
2020-07-15 11:56:22 +0200

    première étape authentification ok

--- src/Entity/Candidate.php
+++ src/Entity/Candidate.php
@@ -3,14 +3,13 @@
 namespace App\Entity;
 
 use App\Repository\CandidateRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=CandidateRepository::class)
  */
-class Candidate
+class Candidate implements UserInterface
 {
     /**
      * @ORM\Id()
@@ -20,299 +19,24 @@ class Candidate
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $gender;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $first_name;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $last_name;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $address;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $country;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $nationality;
-
-    /**
-     * @ORM\Column(type="boolean", nullable=true)
-     */
-    private $passport;
-
-    /**
-     * @ORM\Column(type="string", length=255, nullable=true)
-     */
-    private $passport_file;
-
-    /**
-     * @ORM\Column(type="string", length=255, nullable=true)
-     */
-    private $cv_file;
-
-    /**
-     * @ORM\Column(type="string", length=255, nullable=true)
-     */
-    private $picture;
-
-    /**
-     * @ORM\Column(type="string", length=255, nullable=true)
-     */
-    private $current_location;
-
-    /**
-     * @ORM\Column(type="string", length=255, nullable=true)
-     */
-    private $date_birth;
-
-    /**
-     * @ORM\Column(type="string", length=255, nullable=true)
-     */
-    private $place_birth;
-
-    /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=180, unique=true)
      */
     private $email;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
      */
     private $password;
-
-    /**
-     * @ORM\Column(type="boolean", nullable=true)
-     */
-    private $availability;
-
-    /**
-     * @ORM\Column(type="string", length=255, nullable=true)
-     */
-    private $job_sector;
-
-    /**
-     * @ORM\Column(type="string", length=255, nullable=true)
-     */
-    private $experience;
-
-    /**
-     * @ORM\Column(type="string", length=255, nullable=true)
-     */
-    private $short_description;
-
-    /**
-     * @ORM\Column(type="string", length=255, nullable=true)
-     */
-    private $notes;
-
-    /**
-     * @ORM\Column(type="datetime")
-     */
-    private $created_at;
-
-    /**
-     * @ORM\Column(type="datetime", nullable=true)
-     */
-    private $updated_at;
-
-    /**
-     * @ORM\Column(type="datetime", nullable=true)
-     */
-    private $deleted_at;
-
-    /**
-     * @ORM\Column(type="string", length=255, nullable=true)
-     */
-    private $files;
-
-    /**
-     * @ORM\OneToMany(targetEntity=JobCategory::class, mappedBy="candidate")
-     */
-    private $category_id;
-
-    /**
-     * @ORM\ManyToOne(targetEntity=JobCategory::class)
-     */
-    private $job_category;
-
-    public function __construct()
-    {
-        $this->category_id = new ArrayCollection();
-    }
 
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getGender(): ?string
-    {
-        return $this->gender;
-    }
-
-    public function setGender(string $gender): self
-    {
-        $this->gender = $gender;
-
-        return $this;
-    }
-
-    public function getFirstName(): ?string
-    {
-        return $this->first_name;
-    }
-
-    public function setFirstName(string $first_name): self
-    {
-        $this->first_name = $first_name;
-
-        return $this;
-    }
-
-    public function getLastName(): ?string
-    {
-        return $this->last_name;
-    }
-
-    public function setLastName(string $last_name): self
-    {
-        $this->last_name = $last_name;
-
-        return $this;
-    }
-
-    public function getAddress(): ?string
-    {
-        return $this->address;
-    }
-
-    public function setAddress(string $address): self
-    {
-        $this->address = $address;
-
-        return $this;
-    }
-
-    public function getCountry(): ?string
-    {
-        return $this->country;
-    }
-
-    public function setCountry(string $country): self
-    {
-        $this->country = $country;
-
-        return $this;
-    }
-
-    public function getNationality(): ?string
-    {
-        return $this->nationality;
-    }
-
-    public function setNationality(string $nationality): self
-    {
-        $this->nationality = $nationality;
-
-        return $this;
-    }
-
-    public function getPassport(): ?bool
-    {
-        return $this->passport;
-    }
-
-    public function setPassport(?bool $passport): self
-    {
-        $this->passport = $passport;
-
-        return $this;
-    }
-
-    public function getPassportFile(): ?string
-    {
-        return $this->passport_file;
-    }
-
-    public function setPassportFile(?string $passport_file): self
-    {
-        $this->passport_file = $passport_file;
-
-        return $this;
-    }
-
-    public function getCvFile(): ?string
-    {
-        return $this->cv_file;
-    }
-
-    public function setCvFile(?string $cv_file): self
-    {
-        $this->cv_file = $cv_file;
-
-        return $this;
-    }
-
-    public function getPicture(): ?string
-    {
-        return $this->picture;
-    }
-
-    public function setPicture(?string $picture): self
-    {
-        $this->picture = $picture;
-
-        return $this;
-    }
-
-    public function getCurrentLocation(): ?string
-    {
-        return $this->current_location;
-    }
-
-    public function setCurrentLocation(?string $current_location): self
-    {
-        $this->current_location = $current_location;
-
-        return $this;
-    }
-
-    public function getDateBirth(): ?string
-    {
-        return $this->date_birth;
-    }
-
-    public function setDateBirth(?string $date_birth): self
-    {
-        $this->date_birth = $date_birth;
-
-        return $this;
-    }
-
-    public function getPlaceBirth(): ?string
-    {
-        return $this->place_birth;
-    }
-
-    public function setPlaceBirth(?string $place_birth): self
-    {
-        $this->place_birth = $place_birth;
-
-        return $this;
     }
 
     public function getEmail(): ?string
@@ -327,9 +51,41 @@ class Candidate
         return $this;
     }
 
-    public function getPassword(): ?string
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
     {
-        return $this->password;
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
+    {
+        return (string) $this->password;
     }
 
     public function setPassword(string $password): self
@@ -339,154 +95,20 @@ class Candidate
         return $this;
     }
 
-    public function getAvailability(): ?bool
+    /**
+     * @see UserInterface
+     */
+    public function getSalt()
     {
-        return $this->availability;
-    }
-
-    public function setAvailability(?bool $availability): self
-    {
-        $this->availability = $availability;
-
-        return $this;
-    }
-
-    public function getJobSector(): ?string
-    {
-        return $this->job_sector;
-    }
-
-    public function setJobSector(?string $job_sector): self
-    {
-        $this->job_sector = $job_sector;
-
-        return $this;
-    }
-
-    public function getExperience(): ?string
-    {
-        return $this->experience;
-    }
-
-    public function setExperience(?string $experience): self
-    {
-        $this->experience = $experience;
-
-        return $this;
-    }
-
-    public function getShortDescription(): ?string
-    {
-        return $this->short_description;
-    }
-
-    public function setShortDescription(?string $short_description): self
-    {
-        $this->short_description = $short_description;
-
-        return $this;
-    }
-
-    public function getNotes(): ?string
-    {
-        return $this->notes;
-    }
-
-    public function setNotes(?string $notes): self
-    {
-        $this->notes = $notes;
-
-        return $this;
-    }
-
-    public function getCreatedAt(): ?\DateTimeInterface
-    {
-        return $this->created_at;
-    }
-
-    public function setCreatedAt(\DateTimeInterface $created_at): self
-    {
-        $this->created_at = $created_at;
-
-        return $this;
-    }
-
-    public function getUpdatedAt(): ?\DateTimeInterface
-    {
-        return $this->updated_at;
-    }
-
-    public function setUpdatedAt(?\DateTimeInterface $updated_at): self
-    {
-        $this->updated_at = $updated_at;
-
-        return $this;
-    }
-
-    public function getDeletedAt(): ?\DateTimeInterface
-    {
-        return $this->deleted_at;
-    }
-
-    public function setDeletedAt(?\DateTimeInterface $deleted_at): self
-    {
-        $this->deleted_at = $deleted_at;
-
-        return $this;
-    }
-
-    public function getFiles(): ?string
-    {
-        return $this->files;
-    }
-
-    public function setFiles(?string $files): self
-    {
-        $this->files = $files;
-
-        return $this;
+        // not needed when using the "bcrypt" algorithm in security.yaml
     }
 
     /**
-     * @return Collection|JobCategory[]
+     * @see UserInterface
      */
-    public function getCategoryId(): Collection
+    public function eraseCredentials()
     {
-        return $this->category_id;
-    }
-
-    public function addCategoryId(JobCategory $categoryId): self
-    {
-        if (!$this->category_id->contains($categoryId)) {
-            $this->category_id[] = $categoryId;
-            $categoryId->setCandidate($this);
-        }
-
-        return $this;
-    }
-
-    public function removeCategoryId(JobCategory $categoryId): self
-    {
-        if ($this->category_id->contains($categoryId)) {
-            $this->category_id->removeElement($categoryId);
-            // set the owning side to null (unless already changed)
-            if ($categoryId->getCandidate() === $this) {
-                $categoryId->setCandidate(null);
-            }
-        }
-
-        return $this;
-    }
-
-    public function getJobCategory(): ?JobCategory
-    {
-        return $this->job_category;
-    }
-
-    public function setJobCategory(?JobCategory $job_category): self
-    {
-        $this->job_category = $job_category;
-
-        return $this;
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 }
